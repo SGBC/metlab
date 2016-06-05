SGBC/metlab

commit c64d1df65c180eb3cc2758b14194d113b8472960

Pipeline now ends on failed return value. Resolves #7.

metlab/external.py
@@ -29,25 +29,27 @@ class External(threading.Thread):
             self.log.info("Starting %s" % self.name)
             self.log.info("cmd: %s" % ([self.name] + self.args))
             try:
+                self.started = True
                 if self.args[-1].startswith(">"):
                     self.process = Popen([self.name] + self.args[:-1], stdout=open(self.args[-1][1:], "w"), stderr=PIPE)
                 else:
                     self.process = Popen([self.name] + self.args, stdout=PIPE)
-                self.started = True
                 self.retval = self.process.communicate()[0].strip()
             except Exception as e:
                 self.log.error(e)
             
             if self._stop.isSet():
-                self.log.info("%s aborted" % self.name)
+                self.log.warning("%s aborted" % self.name)
                 self.process.kill()
                 self.status = "aborted"
+            elif self.retval != 0:
+                self.log.error("Failed Running %s" % self.name)
+                self.status = "failed"
             else:
                 self.log.info("Finished Running %s" % self.name)
+                self.status = "completed"
         except Exception as e:
-            self.status = "aborted"
-            print e
-        self.status = "completed"
+            self.status = "failed"
         return self.retval
     
     def stop(self):

metlab/controller.py
@@ -292,8 +292,12 @@ class RunController(threading.Thread):
                     self.current_process = None
                     self.current.join()
                     self.current = None
-                if self.current and self.current.status == 'aborted':
-                    self.log.warning("%s aborted without finishing" % self.current.name)
+                if self.current and self.current.status in ['failed', 'aborted']:
+                    self.log.error("%s %s" % (self.current.name, self.current.status))
+                    self.current_process = None
+                    self.current.join()
+                    self.current = None
+                    self.run_queue.clear()
                 if self.run_queue and not self.current:
                     pid = sorted(self.run_queue.keys())[0]
                     cmd = self.run_queue[pid]

commit 6e83196cfe0ad0503bb4bd1a4ede404426d608af

Fixes bug introduced by c64d1df

metlab/external.py
@@ -34,7 +34,8 @@ class External(threading.Thread):
                     self.process = Popen([self.name] + self.args[:-1], stdout=open(self.args[-1][1:], "w"), stderr=PIPE)
                 else:
                     self.process = Popen([self.name] + self.args, stdout=PIPE)
-                self.retval = self.process.communicate()[0].strip()
+                self.retval = self.process.communicate()[0]
+                self.retval = self.retval.strip() if self.retval else self.retval
             except Exception as e:
                 self.log.error(e)
             
@@ -42,13 +43,14 @@ class External(threading.Thread):
                 self.log.warning("%s aborted" % self.name)
                 self.process.kill()
                 self.status = "aborted"
-            elif self.retval != 0:
-                self.log.error("Failed Running %s" % self.name)
+            elif self.process.returncode != 0:
+                self.log.error("Failed Running %s, retval = '%s'" % (self.name, self.process.returncode))
                 self.status = "failed"
             else:
                 self.log.info("Finished Running %s" % self.name)
                 self.status = "completed"
         except Exception as e:
+            self.log.warning(e)
             self.status = "failed"
         return self.retval
     
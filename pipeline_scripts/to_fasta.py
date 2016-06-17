937849cda32832d99f2b07330b34867b9a773638
Hadrien Gourlé <gourlehadrien@gmail.com>
2016-06-17 14:09:23 +0200

    add fq ext support, fixes #9

--- pipeline_scripts/to_fasta.py
+++ pipeline_scripts/to_fasta.py
@@ -9,16 +9,17 @@ import gzip
 from Bio import SeqIO
 
 def to_fasta(filename, outfile = None):
-    
+
     try:
         file_in = gzip.open(filename) if filename.endswith(".gz") else open(filename)
     except Exception as e:
         sys.stderr.write("ERROR: Couldn't open input file %s\n" % filename)
         sys.stderr.write(str(e) + "\n")
         sys.exit(1)
-    
+
     ext = filename.rstrip('.gz').split('.')[-1]
-    
+    ext = "fastq" if ext in ["fq"] else ext
+
     if not outfile:
         outfile = filename.split("/")[-1].rstrip('.gz')
         outfile = ".".join( outfile.split(".")[:-1] ) + ".fasta"
@@ -35,16 +36,16 @@ def to_fasta(filename, outfile = None):
         sys.exit(1)
 
 if __name__ == '__main__':
-    
+
     import argparse
-    
+
     parser = argparse.ArgumentParser( description = __doc__ )
-    
+
     parser.add_argument("infile", nargs="+", help="Biopython recognized file(s) to convert")
     parser.add_argument("-o", "--output", help="output file name, default is same as in-file")
-    
+
     args = parser.parse_args()
-    
+
     for i, filename in enumerate(args.infile):
         outfile = args.output + ".%i" % (i+1) if len(args.infile) > 1 else args.output
         to_fasta(filename, outfile)
